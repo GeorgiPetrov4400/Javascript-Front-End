c9ac0c3f6bc976b48e3663fbbbe91985c5f730e7
Georgi Petrov <gap4400@gmail.com>
2023-04-10 15:36:31 +0300

    Concat name and age + 5 years

---
+++ concat.js
@@ -0,0 +1,8 @@
+function solve(name, age) {
+    let output = `My name is ${name} and my age is ${age + 5}`;
+    console.log(output);
+}
+
+solve('Joro', 30);
+solve('Gosho', 40);
+solve('Pesho', 50);
