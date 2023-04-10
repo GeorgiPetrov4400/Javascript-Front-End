d7a68a4e6a95f71d7379e39b71d55697214016fb
Georgi Petrov <gap4400@gmail.com>
2023-04-10 18:00:40 +0300

    Destructuring array

---
+++ array.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3];
+let [first, second, third, fourth] = numbers;
+
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(fourth);
+
+let array = [1, 2, 3, ...[4, 5, 6]];
+console.log(array);
