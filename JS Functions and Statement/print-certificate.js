8c270584dbe3de2b59411c2365fbd3782675ba6c
Georgi Petrov <gap4400@gmail.com>
2023-06-06 23:08:53 +0300

    Nested functions

---
+++ JS Functions and Statement/print-certificate.js
@@ -0,0 +1,7 @@
+function printCertificate(grade, [firstName, lastName]) {
+    console.log(grade);
+    console.log(firstName);
+    console.log(lastName);
+}
+
+printCertificate(5.25, ['Joro', 'Jorov']);
