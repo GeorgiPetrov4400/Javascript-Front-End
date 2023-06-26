3eb69d786c14f513e8313d84fcd19d6c61b12c64
Georgi Petrov <gap4400@gmail.com>
2023-06-26 20:10:06 +0300

    Task 5 and 6

--- DOM and Events/06. Add-Delete/app.js
+++ DOM and Events/06. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById('items');
+    const input = document.getElementById('newItemText');
+    const newLi = document.createElement('li');
+    const newAnchor = document.createElement('a');
+    newLi.textContent = input.value;
+    ulContainer.appendChild(newLi);
+    newAnchor.textContent = '[Delete]';
+    newAnchor.addEventListener('click', deleteHandler);
+    newLi.appendChild(newAnchor);
+    input.value = '';
+
+    function deleteHandler(e) {
+        const liItem = e.currentTarget.parentElement;
+        liItem.remove();
+    }
 }
