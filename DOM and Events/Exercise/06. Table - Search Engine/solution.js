8af4923b2db83bfb2e56bf4a9b98c8f0c2b29629
Georgi Petrov <gap4400@gmail.com>
2023-06-28 22:44:04 +0300

    Task 4 and 6

--- DOM and Events/Exercise/06. Table - Search Engine/solution.js
+++ DOM and Events/Exercise/06. Table - Search Engine/solution.js
@@ -1,8 +1,42 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   const searchBtn = document.getElementById('searchBtn');
+   const searchField = document.getElementById('searchField');
+   const allTableRows = Array.from(document.querySelectorAll('tbody tr'));
+   searchBtn.addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let searchFieldValue = searchField.value;
+      for (const tableRow of allTableRows) {
+         if (tableRow.classList.contains('select')) {
+            tableRow.classList.remove('select');
+         }
 
+         if (tableRow.textContent.includes(searchFieldValue)) {
+            tableRow.classList.add('select');
+         }
+      }
    }
+
+   // const searchInput = document.getElementById('searchField');
+   // document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   // function onClick() {
+   //    const searchedWord = searchInput.value;
+   //    const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+
+   //    for (const row of tableRows) {
+   //       debugger;
+   //       let trimmedTextContent = row.textContent.trim();
+
+   //       if (row.classList.contains('select')) {
+   //          row.classList.remove('select');
+   //       }
+
+   //       if (trimmedTextContent.includes(searchedWord.toLowerCase)) {
+   //          row.classList.add('select');
+   //       } 
+   //    }
+
+   //    searchInput.value = '';
+   // }
 }
