f8e70dcb45780e2c490a68ba70820a2d568e466a
Georgi Petrov <gap4400@gmail.com>
2023-06-29 23:11:56 +0300

    Task 7, 8

--- DOM and Events/Exercise/07. Format the Text/solution.js
+++ DOM and Events/Exercise/07. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  const output = document.getElementById('output');
+  const textarea = document.getElementById('input');
+  let sentences = textarea.value.split('. ');
+  sentences.pop();
+
+  while (sentences.length > 0) {
+    let paragraphSentences = sentences.splice(0, 3);
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = paragraphSentences.join('.') + '.';
+    output.appendChild(newParagraph);
+  } 
 }
--- DOM and Events/Exercise/08. Furniture/solution.js
+++ DOM and Events/Exercise/08. Furniture/solution.js
@@ -1,4 +1,77 @@
 function solve() {
+    const [ generateTextArea, buyTextArea ] = Array.from(document.getElementsByTagName('textarea'));
+    const [ generateBtn, buyBtn ] = Array.from(document.getElementsByTagName('button'));
+    const tbody = document.querySelector('.table > tbody');
 
-  //TODO...
+    generateBtn.addEventListener('click', generateHandler);
+    buyBtn.addEventListener('click', buyHandler)
+
+    function generateHandler () {
+      const data = JSON.parse(generateTextArea.value);
+      for (const { img, name, price, decFactor } of data) {
+        const tableRow = createElement('tr', '', tbody);
+        const firstColumnId = createElement('td', '', tableRow);
+        createElement('img', '', firstColumnId, '', '', {src: img});
+        const secondColumnId = createElement('td', '', tableRow);
+        createElement('p', name, secondColumnId);
+        const thirdColumnId = createElement('td', '', tableRow);
+        createElement('p', price, thirdColumnId);
+        const fourthColumnId = createElement('td', '', tableRow);
+        createElement('p', decFactor, fourthColumnId);
+        const fifthColumnId = createElement('td', '', tableRow);
+        createElement('input', '', fifthColumnId, '', '', { type: 'checkbox' });
+      }
+    }
+
+    function buyHandler() {
+      const allCheckedInputs = Array.from(document.querySelectorAll('tbody tr input: checked'));
+      let boughtItems = [];
+      let totalPrice = 0;
+      let totalDecFactor = 0;
+
+      for (const input of allCheckedInputs) {
+        const tableRow = input.parentElement.parentElement;
+        const [firstColumn, secondColumn, thirdColumn, fourthColumn] = Array.from(tableRow.children);
+        let item = secondColumn.children[0].textContent;
+        boughtItems.push(item);
+        let currentPrice = Number(thirdColumn.children[0].textContent);
+        totalPrice += currentPrice;
+        let currentDecFactor = Number(fourthColumn.children[0].textContent);
+        totalDecFactor += currentDecFactor;
+      }
+
+      buyTextArea.value += `Bought furniture: ${boughtItems.join(', ')}\n`;
+      buyTextArea.value += `Total price: ${totalPrice.toFixed(2)}\n`;
+      buyTextArea.value += `Average decoration factor: ${totalDecFactor / allCheckedInputs.length}`;
+    }
+}
+
+function createElement(type, content, id, classes, attributes) {
+  const htmlElement = document.createElement(type);
+
+  if (content && type !== 'input') {
+    htmlElement.textContent = content;
+  }
+
+  if (content && type === 'input') {
+    htmlElement.value = content;
+  }
+
+  if (id) {
+    htmlElement.id = id;
+  }
+
+  if (classes) {
+    htmlElement.classList.add(...classes);
+  }
+
+  if (parentNode) {
+    parentNode.appendChild(htmlElement);
+  }
+
+  if (attributes) {
+    for (const key in attributes) {
+        htmlElement.setAttribute(key, attributes[key]);
+    }
+  }
 }
