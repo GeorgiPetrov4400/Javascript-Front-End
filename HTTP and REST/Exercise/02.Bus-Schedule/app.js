4e0db5f71c7be8daca7aeb5bdb899833d547943d
Georgi Petrov <gap4400@gmail.com>
2023-07-11 00:03:27 +0300

    02. Bus-Schedule

--- HTTP and REST/Exercise/02.Bus-Schedule/app.js
+++ HTTP and REST/Exercise/02.Bus-Schedule/app.js
@@ -1,10 +1,34 @@
 function solve() {
+    const busContainer = document.querySelector('#info > span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let nextStopId = 'depot';
+    let stopName = null;
+
     function depart() {
-        // TODO: 
+        arriveBtn.disabled = false;
+        departBtn.disabled = true;
+        fetch(`${BASE_URL}${nextStopId}`)
+            .then((res) => res.json())
+            .then((nextStopInfo) => {
+                const { name, next } = nextStopInfo;
+                busContainer.textContent = `Next stop ${name}`;
+                nextStopId = next;
+                stopName = name;
+                console.log(nextStopInfo);
+            })
+            .catch(() => {
+                busContainer.textContent = 'Error';
+                arriveBtn.disabled = true;
+                departBtn.disabled = true;
+            })
     }
 
     async function arrive() {
-        // TODO:
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
+        busContainer.textContent = `Arriving at ${stopName}`;
     }
 
     return {
